c7345a91a5f1ba3535a3a94a60678d281e16d73d
jscottmatthews <jscott.matthews@gmail.com>
2021-07-19 20:44:10 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,44 +5,69 @@ variable "region" {
 }
 
 variable "zone" {
-  default = "us-cenral1-a"
+  default = "us-central1-a"
+}
+
+variable "cluster_name" {
+  default = "test-cluster"
 }
 
 variable "project" {
   description = "Project ID"
-}
-
-variable "credentials" {
-  default = "key.json"
+  type        = string
 }
 
 
 #provider
 
 provider "google" {
-    project     = var.project
-    region      = var.region
-    zone        = var.zone
-    credentials = var.credentials
+  credentials = "./key.json"
+  region      = var.region
+  zone        = var.zone
+  project     = var.project
 }
 
+data "google_client_config" "provider" {}
 
-#infra
+provider "kubernetes" {}
 
-resource "google_service_account" "sa" {
-    account_id = "gke-sa"
+
+#net / subnet
+
+resource "google_compute_network" "custom" {
+  name                    = "gke-network"
+  auto_create_subnetworks = false
 }
 
-resource "google_container_cluster" "gke_cluster" {
-    name                     = "gke-cluster"
-    location                 = var.region
-    initial_node_count       = 1
+resource "google_compute_subnetwork" "custom" {
+  name          = "gke-subnetwork"
+  ip_cidr_range = "10.10.0.0/16"
+  region        = var.region
+  network       = google_compute_network.custom.id
+  secondary_ip_range = [
+    {
+      range_name    = "services-range"
+      ip_cidr_range = "10.20.0.0/24"
+    },
+    {
+      range_name    = "pod-range"
+      ip_cidr_range = "10.30.0.0/20"
+    }
+  ]
+}
 
-    network     = "default"
-    subnetwork  = "default"
+#cluster
 
-    ip_allocation_policy {
-        cluster_ipv4_cidr_block   = "/16"
-        services_ipv4_cidr_block  = "/22"
-    } 
-} 
+resource "google_container_cluster" "vpc_native_cluster" {
+  name               = var.cluster_name
+  location           = var.region
+  initial_node_count = 1
+
+  network    = google_compute_network.custom.id
+  subnetwork = google_compute_subnetwork.custom.id
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pod-range"
+    services_secondary_range_name = "services-range"
+  }
+}
